0558c4054cf09fed65e54f270a365daf4ae73217
bottledsunrise <askye701@gmail.com>
2026-04-02 17:25:44 -0400

    formatting

--- models/faculty.php
+++ models/faculty.php
@@ -1,101 +1,116 @@
 <?php
 
-class Faculty {
-
+class Faculty{
     private $id, $name, $email;
-
-    public function __construct($name, $email, $id) {
+    
+    public function __construct($name, $email, $id){
         $this->set_name($name);
         $this->set_email($email);
         $this->set_id($id);
     }
-
-    public function set_name($name) { $this->name = $name; }
-    public function set_email($email) { $this->email = $email; }
-    public function set_id($id) { $this->id = $id; }
-
-    public function get_name() { return $this->name; }
-    public function get_email() { return $this->email; }
-    public function get_id() { return $this->id; }
+    
+    public function set_name($name){
+        $this->name = $name;
+    }
+    
+    public function set_email($email){
+        $this->email = $email;
+    }
+    
+    public function set_id($id){
+        $this->id = $id;
+    }
+    
+    public function get_name(){
+        return $this->name;
+    }
+    
+    public function get_email(){
+        return $this->email;
+    }
+    
+    public function get_id(){
+        return $this->id;
+    }
 }
 
-function get_faculty($id) {
+function get_faculty($id){
     global $database;
-
-    $query = 'SELECT id, name, email
-              FROM faculty
-              WHERE id = :id';
-
+    
+    $query = 'SELECT id, name, email FROM faculty WHERE id = :id';
+    
     $statement = $database->prepare($query);
-    $statement->bindValue(':id', $id);
+    $statement->bindValue(":id", $id);
     $statement->execute();
-
+    
     $faculty = $statement->fetch();
+    
     $statement->closeCursor();
-
-    return new Faculty($faculty['name'], $faculty['email'], $faculty['id']);
-}
-
-function list_faculty() {
-    global $database;
-
-    $query = 'SELECT id, name, email FROM faculty';
-    $statement = $database->prepare($query);
-    $statement->execute();
-
-    $faculty_rows = $statement->fetchAll();
-    $statement->closeCursor();
-
-    $faculty_array = array();
-
-    foreach ($faculty_rows as $faculty) {
-        $faculty_array[] = new Faculty(
+    
+    return new Faculty(
             $faculty['name'],
             $faculty['email'],
             $faculty['id']
+    );
+}
+
+function list_faculty(){
+    global $database;
+    
+    $query = 'SELECT id, name, email FROM faculty';
+    
+    $statement = $database->prepare($query);
+    $statement->execute();
+    
+    $faculty_rows = $statement->fetchAll();
+   
+    $statement->closeCursor();
+    
+    $faculty_array = array();
+    
+    foreach($faculty_rows as $faculty){
+        $faculty_array[] = new Faculty(
+                $faculty['name'],
+                $faculty['email'],
+                $faculty['id']
         );
     }
-
+    
     return $faculty_array;
 }
 
-function insert_faculty($faculty) {
+function insert_faculty($faculty){
     global $database;
+    
+    $query = "INSERT INTO faculty (name, email) VALUES (:name, :email)";
+        
+        $statement = $database->prepare($query);
+        $statement->bindValue(":name", $faculty->get_name());
+        $statement->bindValue(":email", $faculty->get_email());
+        $statement->execute();
+        $statement->closeCursor();
+}
 
-    $query = "INSERT INTO faculty (name, email)
-              VALUES (:name, :email)";
-
+function update_faculty($faculty){
+    global $database;
+    
+    $query = "UPDATE faculty SET name = :name, email = :email WHERE id = :id";
+    
     $statement = $database->prepare($query);
-    $statement->bindValue(':name', $faculty->get_name());
-    $statement->bindValue(':email', $faculty->get_email());
+    $statement->bindValue(":name", $faculty->get_name());
+    $statement->bindValue(":email", $faculty->get_email());
+    $statement->bindValue(":id", $faculty->get_id());
     $statement->execute();
     $statement->closeCursor();
 }
 
-function update_faculty($faculty) {
+function delete_faculty($id){
     global $database;
-
-    $query = "UPDATE faculty
-              SET name = :name,
-                  email = :email
-              WHERE id = :id";
-
+    
+    $query = "DELETE FROM faculty WHERE id = :id";
+    
     $statement = $database->prepare($query);
-    $statement->bindValue(':name', $faculty->get_name());
-    $statement->bindValue(':email', $faculty->get_email());
-    $statement->bindValue(':id', $faculty->get_id());
-    $statement->execute();
-    $statement->closeCursor();
-}
-
-function delete_faculty($id) {
-    global $database;
-
-    $query = "DELETE FROM faculty
-              WHERE id = :id";
-
-    $statement = $database->prepare($query);
-    $statement->bindValue(':id', $id);
+    $statement->bindValue(":id", $id);
     $statement->execute();
     $statement->closeCursor();
 }
--- models/section.php
+++ models/section.php
@@ -1,122 +1,113 @@
 <?php
 
-class Section {
-
+class Section{
     private $course_code, $faculty_id, $semester, $id;
-
-    public function __construct($course_code, $faculty_id, $semester, $id) {
+    
+    public function __construct($course_code, $faculty_id, $semester, $id){
         $this->set_course_code($course_code);
         $this->set_faculty_id($faculty_id);
         $this->set_semester($semester);
         $this->set_id($id);
     }
-
-    public function set_course_code($course_code) {
+    
+    public function set_course_code($course_code){
         $this->course_code = $course_code;
     }
-
-    public function set_faculty_id($faculty_id) {
+    
+    public function set_faculty_id($faculty_id){
         $this->faculty_id = $faculty_id;
     }
-
-    public function set_semester($semester) {
+    
+    public function set_semester($semester){
         $this->semester = $semester;
     }
-
-    public function set_id($id) {
+    
+    public function set_id($id){
         $this->id = $id;
     }
-
-    public function get_course_code() {
+    
+    public function get_course_code(){
         return $this->course_code;
     }
-
-    public function get_faculty_id() {
+    
+    public function get_faculty_id(){
         return $this->faculty_id;
     }
-
-    public function get_semester() {
+    
+    public function get_semester(){
         return $this->semester;
     }
-
-    public function get_id() {
+    
+    public function get_id(){
         return $this->id;
     }
 }
 
-function get_section($id) {
+function get_section($id){
     global $database;
-
-    $query = 'SELECT id, course_code, faculty_id, semester
-              FROM section
-              WHERE id = :id';
-
+    
+    $query = 'SELECT id, course_code, faculty_id, semester FROM section WHERE id = :id';
+    
     $statement = $database->prepare($query);
     $statement->bindValue(":id", $id);
     $statement->execute();
-
+    
     $section = $statement->fetch();
-
+    
     $statement->closeCursor();
-
+    
     return new Section(
-        $section['course_code'],
-        $section['faculty_id'],
-        $section['semester'],
-        $section['id']
-    );
-}
-
-function list_sections() {
-    global $database;
-
-    $query = 'SELECT id, course_code, faculty_id, semester
-              FROM section';
-
-    $statement = $database->prepare($query);
-    $statement->execute();
-
-    $sections = $statement->fetchAll();
-
-    $statement->closeCursor();
-
-    $sections_array = array();
-
-    foreach ($sections as $section) {
-        $sections_array[] = new Section(
             $section['course_code'],
             $section['faculty_id'],
             $section['semester'],
             $section['id']
+    );
+}
+
+function list_sections(){
+    global $database;
+    
+    $query = 'SELECT id, course_code, faculty_id, semester FROM section';
+    
+    $statement = $database->prepare($query);
+    $statement->execute();
+    
+    $sections = $statement->fetchAll();
+   
+    $statement->closeCursor();
+    
+    $sections_array = array();
+    
+    foreach($sections as $section){
+        $sections_array[] = new Section(
+                $section['course_code'],
+                $section['faculty_id'],
+                $section['semester'],
+                $section['id']
         );
     }
-
+    
     return $sections_array;
 }
 
-function insert_section($section) {
+function insert_section($section){
     global $database;
-
-    $query = "INSERT INTO section (course_code, faculty_id, semester)
-              VALUES (:course_code, :faculty_id, :semester)";
-
-    $statement = $database->prepare($query);
-    $statement->bindValue(":course_code", $section->get_course_code());
-    $statement->bindValue(":faculty_id", $section->get_faculty_id());
-    $statement->bindValue(":semester", $section->get_semester());
-    $statement->execute();
-    $statement->closeCursor();
+    
+    $query = "INSERT INTO section (course_code, faculty_id, semester) VALUES (:course_code, :faculty_id, :semester)";
+        
+        $statement = $database->prepare($query);
+        $statement->bindValue(":course_code", $section->get_course_code());
+        $statement->bindValue(":faculty_id", $section->get_faculty_id());
+        $statement->bindValue(":semester", $section->get_semester());
+        $statement->execute();
+        $statement->closeCursor();
 }
 
-function update_section($section) {
+function update_section($section){
     global $database;
-
-    $query = "UPDATE section
-              SET course_code = :course_code,
-                  faculty_id = :faculty_id,
-                  semester = :semester
-              WHERE id = :id";
-
+    
+    $query = "UPDATE section SET course_code = :course_code, faculty_id = :faculty_id, semester = :semester WHERE id = :id";
+    
     $statement = $database->prepare($query);
     $statement->bindValue(":course_code", $section->get_course_code());
     $statement->bindValue(":faculty_id", $section->get_faculty_id());
@@ -126,12 +117,11 @@ function update_section($section) {
     $statement->closeCursor();
 }
 
-function delete_section($id) {
+function delete_section($id){
     global $database;
-
-    $query = "DELETE FROM section
-              WHERE id = :id";
-
+    
+    $query = "DELETE FROM section WHERE id = :id";
+    
     $statement = $database->prepare($query);
     $statement->bindValue(":id", $id);
     $statement->execute();
